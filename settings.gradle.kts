36623d825882de8c8af47dbdf01596c7a308ba26
SenriYasuda <ysd1000ri@gmail.com>
2024-04-24 09:19:25 +0900

    firest_practice_commitg30424

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "rensyuG2_0420"
+include(":app")
+ 
